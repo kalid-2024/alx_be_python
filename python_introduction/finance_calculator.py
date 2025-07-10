47309e6d3088729e09033ece70dd0c17cd582930
kalid-2024 <kalidmohammed236@gmail.com>
2025-07-10 18:37:18 +0300

    annual savings calculator including 5 percent interest rate and monthly savings calculator.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+income = int(input('Enter your monthly income: '))
+expenses = int(input('Enter your total monthly expenses: '))
+savings = income - expenses
+i = 0.05
+projected_savings = (savings) * 12 + (savings * 12 * i)
+print(f'Your monthly savings are, ${savings}')
+print(f'Projected savings after one year, with interest, is: ${projected_savings}')
