b33d3d1c2b5082aa6a2689ee6d10c957386792dc
kalid-2024 <kalidmohammed236@gmail.com>
2025-08-03 22:46:54 +0300

    feat(Correction):corrected the syntax of the method names

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,37 +6,21 @@ class Test_Calculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add_positive(self):
+    def test_addition(self):
         result = self.calc.add(5, 3)
         self.assertEqual(result, 8)
 
-    def test_add_negative(self):
-        result = self.calc.add(-2, 7)
-        self.assertEqual(result, 5)
-
-    def test_subtract_postive(self):
+    def test_subtraction(self):
         result = self.calc.subtract(9,2)
         self.assertEqual(result, 7)
 
-    def test_subtract_negative(self):
-        result = self.calc.subtract(-9,2)
-        self.assertEqual(result, -11)
-    
-    def test_multiply_positive(self):
+    def test_multiply(self):
         result = self.calc.multiply(4, 3)
         self.assertEqual(result, 12)
 
-    def test_multiply_negative(self):
-        result = self.calc.multiply(-4, 2)
-        self.assertEqual(result, -8)
-
-    def test_divide_normal(self):
+    def test_divide(self):
         result = self.calc.divide(10, 2)
         self.assertEqual(result, 5.0)
-
-    def test_divide_negative(self):
-        result = self.calc.divide(-10, 2)
-        self.assertEqual(result, -5.0)
 
     def test_divide_by_zero(self):
         with self.assertRaises(ZeroDivisionError):
