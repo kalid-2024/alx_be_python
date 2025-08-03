d0c9ad899b5e50271cc165491d9a7d887b30f386
kalid-2024 <kalidmohammed236@gmail.com>
2025-08-03 23:51:28 +0300

    feat(corrected): correction

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,27 +1,47 @@
 class Book:
-    public title,author
-    private _is_checked_out
+    title,author
+    _is_checked_out
     def __init__(self,title,author):
         self.title = title
         self.author = author
         self._is_checked_out = False 
+    
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
 
-class Library(Book):
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
+
+    def is_available(self):
+        return not self._is_checked_out
+
+class Library:
     def __init__(self):
         self._books = []
-    def add_book(self):
-        private_books.append(self.title)
+
+    def add_book(self,book):
+        _books.append(book)
+        print(f'Available books after setup:{self.title} by {self.author}')
         print(f'{self.title} by {self.author}')
 
     def check_out_book(self,title): 
         private_books.remove(self.title)
         self._is_checked_out = True
+        print(f"Available books after checking out {self.title}:")
 
-    def return_book(self,title):
-        private_books.append(self.title)
+    def return_book(self,book):
+        private_books.append(book.title)
         self._is_checked_out = False
-        print(f'{self.title} by {self.author}')
-
+        print(f"Available books after returning {self.title}:")
+          for book in private_books:
+            print(f'{book.title} by {book.author}')
+      
     def list_available_books(self):
         for book in private_books:
             print(book)
