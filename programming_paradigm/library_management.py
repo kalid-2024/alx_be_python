c8a52a80c071313cb31e928750096b8ebdbc2629
kalid-2024 <kalidmohammed236@gmail.com>
2025-08-09 13:55:30 +0300

    feat(added): a magic method str to the code for string representation of the object

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,6 +1,4 @@
 class Book:
-    title,author
-    _is_checked_out
     def __init__(self,title,author):
         self.title = title
         self.author = author
@@ -21,27 +19,26 @@ class Book:
     def is_available(self):
         return not self._is_checked_out
 
+    def __str__(self):  
+        return f"{self.title} by {self.author}"
+
 class Library:
     def __init__(self):
         self._books = []
 
     def add_book(self,book):
-        _books.append(book)
-        print(f'Available books after setup:{self.title} by {self.author}')
-        print(f'{self.title} by {self.author}')
-
+        self._books.append(book)
+        
     def check_out_book(self,title): 
-        private_books.remove(self.title)
-        self._is_checked_out = True
-        print(f"Available books after checking out {self.title}:")
-
-    def return_book(self,book):
-        private_books.append(book.title)
-        self._is_checked_out = False
-        print(f"Available books after returning {self.title}:")
-          for book in private_books:
-            print(f'{book.title} by {book.author}')
-      
+        for book in self._books:
+            if book.title == title and book.is_available():
+                book.check_out()
+       
+    def return_book(self,title):
+        for book in self._books:
+            if book.title == title and not book.is_available():
+                book.return_book()
+        
     def list_available_books(self):
-        for book in private_books:
+        for book in self._books:
             print(book)
